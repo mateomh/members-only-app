a5dc9ac62195702cf7ec45df4ed47332e1a2cc93
Jose Pedraza <pastorpedraza3@gmail.com>
2020-09-09 16:54:59 -0500

    fix login and logout for users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :get_user
 
   # GET /posts
   # GET /posts.json
@@ -70,5 +71,9 @@ class PostsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:content)
+    end
+
+    def get_user
+      @user = current_user
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :posts, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "posts#index"
+
+
 end
